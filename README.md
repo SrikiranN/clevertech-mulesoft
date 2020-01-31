696fd52b6bba04504e0a38ec14a9804ca3b170d1
Iwerson Souza <iwerson@gmail.com>
2020-01-31 15:24:44 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# MuleSoft challenge
+
+This challenge consists of implementing the incomplete flows in the project.
+Both have to be created in the same project using Anypoint Studio, the IDE for Mule ESB (https://developer.mulesoft.com/). 
+You can download it from https://www.mulesoft.com/lp/dl/studio. 
+Make sure you use Anypoint Studio 7 and Mule 4.
+
+# Tips
+* Explore the MUnit flows as these will help validate your implementation.
+* Feel free to adjust the project to meet best practices and standards to take the most out of the MuleSoft components.
+
+# Format
+* This assessment must be delivered within 2 days.
+* If you wish, you can provide a IMPL.txt (plain text) or a IMPL.md (Markdown) file at the root of your repository, explaining:
+    * Any comments regarding your implementation.
+    * Mention anything that was asked but not delivered and why.
+* Any questions, please send an email to your recruiter.
+
+### Flows ###
+
+#1 - countryFlow
+
+ * Summary: Read a record from a file based on an URI parameter, call a REST enpoint and return the expected response
+ * URI Params: countryID
+ * Resources: resources/endpoints/country/country.csv
+ * REST: https://restcountries.eu/rest/v2/alpha/{code}
+
+#2 - currencyFlow
+
+ * Summary: Call two SOAP endpoints based on the query parameters and return the expected response
+ * Query Params: currency, date
+ * SOAP: http://currencyconverter.kowabunga.net/converter.asmx (GetCurrencyRate, GetCultureInfo)
+
+#3 - employeeFlow
+
+ * Summary: Load an in-memory database, query it based on the query params and return the expected response
+ * Query Params: country, ageGroup, worldRank
+ * Resources: resources/endpoints/employee/employee.sql
+ * Query Details: 
+	age_group: 
+		age between 18 and 21 = A
+		age between 22 and 25 = B
+		age between 26 and 29 = C
+		age greater than 29   = D
+		otherwise             = ?		
+	country_rank: ordinal position of the employee according to his/her salary in comparison to other employee's salary on the same country
+	country_salary_percent: % of the employee salary in comparison to the sum of all employee's salary on the same country (format: 2 decimals + %)
+	world_rank: ordinal position of the employee according to his/her salary over the whole database  
+ * In-Memory Database: http://www.h2database.com/html/main.html
+ * Tip: INIT statement may be useful in a jdbc connection url
+
+#4 - sortFlow
+
+ * Summary: Sort an array of numbers based on the query param by using Java or other scripting language and return the expected response
+ * Query Params: inputArray  
+
+Thank you,
+Clevertech Team
---
+++ src/main/resources/endpoints/employee/employee.sql
@@ -0,0 +1,41 @@
+CREATE TABLE IF NOT EXISTS employees (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	age INT NOT NULL,
+	country VARCHAR NOT NULL,
+	education VARCHAR NOT NULL,
+	occupation VARCHAR NOT NULL, 
+	salary DECIMAL(8,2) NOT NULL
+);
+
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Rosie', 'Harper',30,'United States','Primary','Graphic Designer',56114);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Alfred', 'Kelley',18,'Canada','Master','Singer',42629);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('George', 'Anderson',21,'Finland','Lower secondary','Pharmacist',43347);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Grace', 'Allen',28,'Brazil','Primary','Producer',111431);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Robert', 'Evans',26,'United States','Bachelor','Agronomist',176085);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Carlos', 'Montgomery',19,'Iceland','Upper secondary','Graphic Designer',35557);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Brooke', 'Hawkins',28,'Brazil','Bachelor','Mathematician',193277);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Stella', 'Harrison',20,'South Africa','Primary','Architect',193670);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Carina', 'Gray',19,'Canada','Bachelor','Insurer',196166);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Harold', 'Barrett',25,'Iceland','Doctoral','Firefighter',51662);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Victor', 'Robinson',29,'United States','Doctoral','Dancer',65237);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Paige', 'Morgan',22,'South Africa','Master','Chemist',108263);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Alisa', 'Watson',22,'Finland','Master','Programmer',62301);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Natalie', 'Adams',18,'United States','Upper secondary','Producer',169101);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Mike', 'Chapman',23,'Iceland','Lower secondary','Driver',65828);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Annabella', 'Davis',29,'South Africa','Lower secondary','Composer',95998);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Eleanor', 'Chapman',22,'United States','Master','Interior Designer',181247);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Dexter', 'Anderson',24,'Canada','Lower secondary','Biochemist',133004);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Sawyer', 'Payne',30,'United States','Bachelor','Carpenter',71233);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Chelsea', 'Riley',19,'Brazil','Master','Astronomer',131137);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Arthur', 'Owens',19,'Iceland','Primary','Producer',67300);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Carl', 'Reed',26,'South Africa','Bachelor','Baker',180896);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Elian', 'Fowler',20,'United States','Upper secondary','Jeweller',174740);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Julia', 'Turner',29,'Brazil','Doctoral','Architect',81691);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Sarah', 'Farrell',28,'Canada','Upper secondary','Mechanic',106762);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Alina', 'Tucker',23,'Iceland','Doctoral','Lawer',145349);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Miller', 'Walker',26,'United States','Upper secondary','Police Officer',165328);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Cherry', 'Jones',19,'South Africa','Primary','Accountant',33296);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Kevin', 'Carroll',30,'Canada','Master','Electrician',56561);
+INSERT INTO employees (first_name, last_name, age, country, education, occupation, salary) VALUES ('Alexander', 'Richardson',28,'Finland','Upper secondary','Cook',102220);
